eabcd3f63cfdcfba4e7106f5edccf0d298f51a90
Ho0dLuM <Jordon.Hoshor@gmail.com>
2016-10-30 22:53:17 -0600

    can read file from directive and pass to controller to disply on page, that is something

--- src/components/main/main.directive.js
+++ src/components/main/main.directive.js
@@ -6,8 +6,23 @@
     .module('inputJSON.components.main')
     .directive('fileUpload', fileUpload);
 
-  function fileUpload() {
-
+  function fileUpload($parse) {
+    return {
+      restrict: 'A',
+      scope: false,
+      link: (scope, element, attrs) => {
+        const parse = $parse(attrs.fileUpload);
+        element.on('change', (onChangeEvent) => {
+          const reader = new FileReader();
+          reader.onload = (onLoadEvent) => {
+            scope.$apply(() => {
+              parse(scope, {$fileContent:onLoadEvent.target.result});
+            });
+          };
+          reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+        });
+      }
+    };
   }
 
 })();
