14502cc5b0a3dbc4a16fef534ce93d46e2456f6b
Ho0dLuM <Jordon.Hoshor@gmail.com>
2016-11-01 17:20:47 -0600

    rendering the file to the page with recursive function.

--- src/components/main/main.service.js
+++ src/components/main/main.service.js
@@ -8,6 +8,21 @@
 
   function fileService() {
     /*jshint validthis: true */
+    this.convertHTML = (Obj) => {
+      const fileContent = Obj.content;
+      const htmlTag = Obj.tag;
+      if (this.objCheck(fileContent)) {
+        return (`<${htmlTag}>${this.convertHTML(fileContent)}</${htmlTag}>`);
+      } else if (Array.isArray(fileContent)) {
+        return (`<${htmlTag}>${fileContent.map(section => this.convertHTML(section)).join('')}</${htmlTag}>`);
+      } else {
+        return (`<${htmlTag}>${fileContent}</${htmlTag}>`);
+      }
+    };
+
+    this.objCheck = (content) => {
+      return (typeof content === 'object' && !Array.isArray(content) && content !== null);
+    };
   }
 
 })();
